9136456d348c9bf8d5db5a9a4e07f4f1e4e29e3b
Klimov Paul <klimov.paul@gmail.com>
2018-09-19 12:42:24 +0300

    source code moved to 'src' directory

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -12,6 +12,6 @@ require_once(__DIR__ . '/../vendor/autoload.php');
 require_once(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
 Yii::setAlias('@yii2tech/tests/unit/crontab', __DIR__);
-Yii::setAlias('@yii2tech/crontab', dirname(__DIR__));
+Yii::setAlias('@yii2tech/crontab', dirname(__DIR__) . '/src');
 
 require_once(__DIR__ . '/compatibility.php');
